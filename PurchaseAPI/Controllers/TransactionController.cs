3d58f8179d0b7566398a2e0fa6fb7bd684f3bdb1
Prateek Dev <prateek-dev@pluralsight.com>
2025-10-18 12:18:58 +0530

    Adding change to the docker file

---
+++ PurchaseAPI/Controllers/TransactionController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Purchase.Core.Entities;
+using Purchase.Core.Interfaces;
+
+namespace PurchaseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] PurchaseTransaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdTransaction = await _transactionService.CreateTransactionAsync(transaction);
+            return CreatedAtAction(nameof(GetById), new { id = createdTransaction.Id }, createdTransaction);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var transaction = await _transactionService.GetTransactionAsync(id);
+            return Ok(transaction);
+        }
+
+        //[HttpGet("{id:guid}/convert/{currencyCode}")]
+        //public async Task<IActionResult> GetByIdInCurrency(Guid id, string currencyCode)
+        //{
+        //    // This method assumes your service calls the Treasury API for conversion
+        //    var transactionInCurrency = await _transactionService.GetTransactionInCurrencyAsync(id, currencyCode.ToUpper());
+        //    return Ok(transactionInCurrency);
+        //}
+    }
+}
