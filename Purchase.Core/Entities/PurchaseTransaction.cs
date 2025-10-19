8c7937b5bce82083b94a197e1fdf6b2490467c53
Prateek Dev <prateek-dev@pluralsight.com>
2025-10-19 12:52:19 +0530

    The change is to hide the ID while creating the transaction

--- Purchase.Core/Entities/PurchaseTransaction.cs
+++ Purchase.Core/Entities/PurchaseTransaction.cs
@@ -4,13 +4,15 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Purchase.Core.Entities
 {
     public class PurchaseTransaction
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [SwaggerSchema(ReadOnly = true)]
+        public Guid Id { get; private set; } = Guid.NewGuid();
         [Required]
         [MaxLength(50, ErrorMessage = "Description cannot exceed 50 characters")]
         public string Description { get; set; } = null!;
